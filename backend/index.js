8002d87a84d7214e32c661d7e2d31c5e0b0ff489
Yashas <p.yahha@yahoo.com>
2025-12-14 11:22:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,22 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 
 import interviewRoutes from "./routes/interview.js";
 import feedbackRoutes from "./routes/feedback.js";
-import resumeRoutes from "./routes/resume.js";
-import interviewFlowRoutes from "./routes/interviewFlow.js";
-
-
-
-dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (_, res) => {
+app.use("/interview", interviewRoutes);
+app.use("/feedback", feedbackRoutes);
+
+app.get("/", (req, res) => {
   res.send("InterviewGPT Backend Running");
 });
 
-app.use("/interview", interviewRoutes);
-app.use("/feedback", feedbackRoutes);
-app.use("/resume", resumeRoutes);
-app.use("/interview-flow", interviewFlowRoutes);
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Backend running on port ${PORT}`)
-);
-
-
+app.listen(PORT, () => {
+  console.log("Backend running on port", PORT);
+});
