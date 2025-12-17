a6291962ce1dea415b3c7d0089e3962ff5c93627
Yashas <p.yahha@yahoo.com>
2025-12-17 23:27:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ import interviewRoutes from "./routes/interview.js";
 import feedbackRoutes from "./routes/feedback.js";
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
@@ -16,6 +15,4 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("Backend running on port", PORT);
-});
+app.listen(PORT, () => console.log("Backend running on port", PORT));
